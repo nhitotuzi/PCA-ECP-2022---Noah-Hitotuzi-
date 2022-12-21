2158d19482804065bec1ecae6e046e25e39b831f
nhitotuzi <noahdelimahitotuzi@gmail.com>
2022-12-20 20:30:05 -0400

    Exercícios do dia 20/12/2022

---
+++ 20222220008.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+	int primeiro=1;
+	char res[]="sim";
+	float e1, e2, e3, media;
+	float maior_e1, maior_e2, maior_e3, maior_med;
+	float menor_e1, menor_e2, menor_e3, menor_med;
+
+	while (!strcmp(res, "sim")){
+		printf("Digite a nota do Exercício 1: ");
+		scanf("%f", &e1);
+		printf("Digite a nota do Exercício 2: ");
+		scanf("%f", &e2);
+		printf("Digite a nota do Exercício 3: ");
+		scanf("%f", &e3);
+		media = (e1+e2+e3)/3;
+
+		if (primeiro){
+			maior_e1 = e1;
+			maior_e2 = e2;
+			maior_e3 = e3;
+			maior_med = media;
+			menor_e1 = e1;
+			menor_e2 = e2;
+			menor_e3 = e3;
+			menor_med = media;
+
+			primeiro = 0;
+		}
+		if(e1 > maior_e1) maior_e1 = e1;
+		if(e2 > maior_e2) maior_e2 = e2;
+		if(e3 > maior_e3) maior_e3 = e3;
+		if(media > maior_med) maior_med = media;
+		if(e1 < menor_e1) menor_e1 = e1;
+		if(e2 < menor_e2) menor_e2 = e2;
+		if(e3 < menor_e3) menor_e3 = e3;
+		if(media < menor_med) menor_med = media;
+
+		printf("Deseja inserir as notas de outro aluno? (sim/não) ");
+		scanf("%s", res);
+       	}
+
+	printf("Exercício 1: Maior = %.2f, Menor = %.2f\n", maior_e1, menor_e1);
+	printf("Exercício 2: Maior = %.2f, Menor = %.2f\n", maior_e2, menor_e2);
+	printf("Exercício 3: Maior = %.2f, Menor = %.2f\n", maior_e3, menor_e3);
+	printf("Média: Maior = %.2f, Menor = %.2f\n", maior_med, menor_med);
+
+	return 0;
+}
